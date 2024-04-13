3679559d5684d4d4ad51a79656efe56b5bd79b49
Rogério Silva <64442407+rogeriosilva-ifpi@users.noreply.github.com>
2024-04-13 11:29:29 -0300

    Add files via upload

---
+++ API_SC_01_08Abril/lucas-gomes.js/01_api.js
@@ -0,0 +1,16 @@
+import { question } from "readline-sync";
+
+
+function main(){
+    let novasenha= Number(numero("Escreva sua senha com 6 digitos "))
+    let senhaantiga= Number(numero("Escreva sua senha antiga de 6 digitos "))
+    let senhaatual=  Number(numero("Qual senha você deseja que seja a atual? "))
+     question if {
+        while (_senhaatual !== senhaantiga)
+} 
+
+}  
+    
+
+
+main()
